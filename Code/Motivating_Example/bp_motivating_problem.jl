571deec02c4cde0a758ec238b44b53b15389d84c
Edargorter <zdbowditch777@gmail.com>
2019-07-15 22:30:41 +0200

    Computing examples for test cases.

--- Code/Motivating_Example/bp_motivating_problem.jl
+++ Code/Motivating_Example/bp_motivating_problem.jl
@@ -15,19 +15,33 @@ Random.seed!(Dates.value(convert(Dates.Millisecond, Dates.now())))
 
 config_filename = "config_motivating.txt"
 config = read_config(config_filename)
-parameters_filename = "parameters_1.txt"
+parameters_filename = "test_parameters.txt"
 params = read_parameters(parameters_filename)
 
-##### TEST OPTIMAL SOLUTION FITNESS #####
+##### TEST SOLUTION FITNESS #####
+
+#=
 
 instructions = [1 1 1 1;0 1 1 1;0 0 1 1]
 durations = [4.665, 3.479, 2.427, 1.429]
 #Test optimal solution
 cand = BPS_Program(instructions, durations)
 objective = get_fitness(config, params, cand, false)
-@printf "\nMotivating example (Optimal): %.2f\n" objective
+@printf "\nMotivating example (Optimal): %.3f\n" objective
+
+=#
+
+durations = [6.0, 6.0, 3.519, 2.431, 1.47, 4.046, 2.179, 1.901, 1.891, 1.928, 4.635]
+@printf "\nSum: %d" sum(durations)
+instructions = [1 1 1 0 1 0 1 0 1 0 0; 0 1 1 1 0 1 1 0 1 0 1; 0 0 1 1 1 1 1 1 1 1 1]
+#Test 36-hour solution
+cand = BPS_Program(instructions, durations)
+objective = get_fitness(config, params, cand, false)
+@printf "\n36-hour example: %.3f\n" objective
 
 print("\n")
+
+#=
 
 ### RUN TESTS ###
 
@@ -65,6 +79,7 @@ for p in 1:no_params
 
 end
 
+=#
 #=
 
 
